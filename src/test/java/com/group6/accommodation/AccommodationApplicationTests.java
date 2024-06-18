c6b180d483dca4c189f4d243cb5fc9ae84c26321
TaeMin Kim <gimtaemin36@gmail.com>
2024-06-18 17:17:43 +0900

    init

---
+++ src/test/java/com/group6/accommodation/AccommodationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6.accommodation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
