e464d9e007c58adb88d680e639e03f0f30deb53d
Hyeonjoon <kingpark02@naver.com>
2024-06-19 21:31:14 +0900

    Feat: 단일 숙소 상세 조회

--- src/main/java/com/group6/accommodation/domain/accommodation/converter/AccommodationConverter.java
+++ src/main/java/com/group6/accommodation/domain/accommodation/converter/AccommodationConverter.java
@@ -1,7 +1,9 @@
 package com.group6.accommodation.domain.accommodation.converter;
 
+import com.group6.accommodation.domain.accommodation.model.dto.AccommodationDetailDto;
 import com.group6.accommodation.domain.accommodation.model.dto.AccommodationDto;
 import com.group6.accommodation.domain.accommodation.model.entity.AccommodationEntity;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -27,5 +29,23 @@ public class AccommodationConverter {
 
     public List<AccommodationDto> toDtoList(List<AccommodationEntity> accommodationEntityList) {
         return accommodationEntityList.stream().map(this::toDto).collect(Collectors.toList());
+    }
+
+    public AccommodationDetailDto toDetailDto(AccommodationEntity accommodation) {
+        return AccommodationDetailDto.builder()
+                .id(accommodation.getId())
+                .title(accommodation.getTitle())
+                .address(accommodation.getAddress())
+                .address2(accommodation.getAddress2())
+                .areacode(accommodation.getAreacode())
+                .sigungucode(accommodation.getSigungucode())
+                .category(accommodation.getCategory())
+                .image(accommodation.getImage())
+                .thumbnail(accommodation.getThumbnail())
+                .latitude(accommodation.getLatitude())
+                .longitude(accommodation.getLongitude())
+                .mlevel(accommodation.getMlevel())
+                .tel(accommodation.getTel())
+                .likeCount(accommodation.getLikeCount()).build();
     }
 }
