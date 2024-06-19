8a243b63310d7c5a3885eb998d8f5b29bd2ef859
qudwns017 <qudwns017@naver.com>
2024-06-19 14:12:03 +0900

    Fix: JPA 테이플 생성 오류 해결

--- src/main/java/com/group6/accommodation/domain/reservation/model/entity/ReservationEntity.java
+++ src/main/java/com/group6/accommodation/domain/reservation/model/entity/ReservationEntity.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
@@ -43,7 +44,7 @@ public class ReservationEntity {
 	@Column(name = "price", nullable = false)
 	private Integer price;
 
-	@ColumnDefault("CURRENT_TIMESTAMP")
+	@CreationTimestamp
 	@Column(name = "created_at", nullable = false)
 	private Instant createdAt;
 
