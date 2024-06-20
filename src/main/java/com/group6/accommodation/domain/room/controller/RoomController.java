0868035d172ca7aec0dbba29a1c89d2ef030314d
TaeMin Kim <gimtaemin36@gmail.com>
2024-06-20 10:13:59 +0900

    FEAT: 객실 전체 조회 init

--- src/main/java/com/group6/accommodation/domain/room/controller/RoomController.java
+++ src/main/java/com/group6/accommodation/domain/room/controller/RoomController.java
@@ -1,5 +1,21 @@
 package com.group6.accommodation.domain.room.controller;
 
+import com.group6.accommodation.domain.room.model.entity.RoomEntity;
+import com.group6.accommodation.domain.room.service.RoomService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/open-api")
+@RequiredArgsConstructor
 public class RoomController {
 
+	private final RoomService roomService;
+
+	@GetMapping("/accommodation/{id}/room")
+	public List<RoomEntity> findAllRooms(@PathVariable Long id) {
+		return roomService.findAllRooms(id);
+	}
 }
--- src/main/java/com/group6/accommodation/domain/room/service/RoomService.java
+++ src/main/java/com/group6/accommodation/domain/room/service/RoomService.java
@@ -1,5 +1,18 @@
 package com.group6.accommodation.domain.room.service;
 
+import com.group6.accommodation.domain.room.model.entity.RoomEntity;
+import com.group6.accommodation.domain.room.repository.RoomRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class RoomService {
 
+	private final RoomRepository roomRepository;
+
+	public List<RoomEntity> findAllRooms(Long accommodationId) {
+		return roomRepository.findAll();
+	}
 }
