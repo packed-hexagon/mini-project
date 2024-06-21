6fe3f208bf83b267407fb8c21a769402e65c43b8
jisun.park <pjs981114@naver.com>
2024-06-21 14:12:26 +0900

    Feat: 조회 기능까지 완성

--- src/main/java/com/group6/accommodation/domain/accommodation/repository/AccommodationRepository.java
+++ src/main/java/com/group6/accommodation/domain/accommodation/repository/AccommodationRepository.java
@@ -1,9 +1,12 @@
 package com.group6.accommodation.domain.accommodation.repository;
 
 import com.group6.accommodation.domain.accommodation.model.entity.AccommodationEntity;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccommodationRepository extends JpaRepository<AccommodationEntity, Long> {
 
-
+    Page<AccommodationEntity> findByIdIn(List<Long> ids, Pageable pageable);
 }
--- src/main/java/com/group6/accommodation/domain/likes/controller/UserLikeController.java
+++ src/main/java/com/group6/accommodation/domain/likes/controller/UserLikeController.java
@@ -1,17 +1,20 @@
 package com.group6.accommodation.domain.likes.controller;
 
+import com.group6.accommodation.domain.accommodation.model.dto.AccommodationDto;
+import com.group6.accommodation.domain.accommodation.model.dto.PagedDto;
 import com.group6.accommodation.domain.likes.model.dto.UserLikeResponseDto;
 import com.group6.accommodation.domain.likes.service.UserLikeService;
-import com.group6.accommodation.global.util.Response;
 import com.group6.accommodation.global.util.ResponseApi;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/user-like")
@@ -28,8 +31,7 @@ public class UserLikeController {
 //        @AuthenticationPrincipal User user
     ) {
 //        var loginUserId = Long.parseLong(user.getUserId());
-        var addLike = userLikeService.addLike(accommodationID, 1L);
-        ResponseApi<UserLikeResponseDto> response = ResponseApi.success(HttpStatus.CREATED, addLike);
+        ResponseApi<UserLikeResponseDto> response = userLikeService.addLike(accommodationID, 1L);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
@@ -40,9 +42,19 @@ public class UserLikeController {
 //        @AuthenticationPrincipal User user
     ) {
 //        var loginUserId = Long.parseLong(user.getUserId());
-        userLikeService.cancelLike(accommodationID, 1L);
-        ResponseApi<String> response = ResponseApi.success(HttpStatus.NO_CONTENT, "Delete Success");
+        ResponseApi<String> response = userLikeService.cancelLike(accommodationID, 1L);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    // 찜한 숙박 목록
+    @GetMapping
+    public ResponseEntity<ResponseApi<PagedDto<AccommodationDto>>> getLikedAccommodation(
+//        @AuthenticationPrincipal User user
+        @RequestParam(name ="page", defaultValue = "0") int page,
+        @RequestParam(name = "size", defaultValue = "5") int size
+    ) {
+//        var loginUserId = Long.parseLong(user.getUserId());
+        ResponseApi<PagedDto<AccommodationDto>> response = userLikeService.getLikedAccommodation(1L, page, size);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 }
