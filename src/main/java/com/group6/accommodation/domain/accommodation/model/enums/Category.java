c827325e2c01d7b9e0455e74dcbe774e2c6e3c17
Hyeonjoon <kingpark02@naver.com>
2024-06-21 16:08:10 +0900

    Feat: Enum 클래스 설정

--- src/main/java/com/group6/accommodation/domain/accommodation/model/enums/Category.java
+++ src/main/java/com/group6/accommodation/domain/accommodation/model/enums/Category.java
@@ -1,24 +1,45 @@
 package com.group6.accommodation.domain.accommodation.model.enums;
 
+import com.group6.accommodation.global.exception.error.AccommodationErrorCode;
+import com.group6.accommodation.global.exception.type.AccommodationException;
 import lombok.Getter;
 
 @Getter
 public enum Category {
-    B02010100("관광호텔"),
-    B02010500("콘도미니엄"),
-    B02010600("유스호스텔"),
-    B02010700("펜션"),
-    B02010900("모텔"),
-    B02011000("민박"),
-    B02011100("게스트하우스"),
-    B02011200("홈스테이"),
-    B02011300("서비스드레지던스"),
-    B02011600("한옥");
+    HOTEL("B02020200", "관광호텔"),
+    CONDO("B02010500", "콘도미니엄"),
+    HOSTEL("B02010600", "유스호스텔"),
+    PENSION("B02010700", "펜션"),
+    MOTEL("B02010800", "모텔"),
+    MINBAK("B02010900", "민박"),
+    GUESTHOUSE("B02011000", "게스트하우스"),
+    HOMESTAY("B02011100", "홈스테이"),
+    RESIDENCE("B02011200", "서비스드레지던스"),
+    HANOK("B02011300", "한옥");
 
-    private final String description;
+    private final String code;
+    private final String name;
 
-    Category(String description) {
-        this.description = description;
+    Category(String code, String name) {
+        this.code = code;
+        this.name = name;
     }
 
+    public static String getNameByCode(String code) {
+        for (Category category : values()) {
+            if (category.code.equals(code)) {
+                return category.name;
+            }
+        }
+        throw new AccommodationException(AccommodationErrorCode.NOT_FOUND_CATEGORY);
+    }
+
+    public static String getCodeByName(String name) {
+        for (Category category : values()) {
+            if (category.name.equals(name)) {
+                return category.code;
+            }
+        }
+        throw new AccommodationException(AccommodationErrorCode.NOT_FOUND_CATEGORY);
+    }
 }
