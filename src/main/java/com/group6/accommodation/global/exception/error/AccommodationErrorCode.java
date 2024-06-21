343dc586fdd52346c34cea5fe4a4600916f497c9
Hyeonjoon <kingpark02@naver.com>
2024-06-21 16:08:54 +0900

    Feat: 데이터 없는 페이지일 경우 에러처리

--- src/main/java/com/group6/accommodation/global/exception/error/AccommodationErrorCode.java
+++ src/main/java/com/group6/accommodation/global/exception/error/AccommodationErrorCode.java
@@ -9,6 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum AccommodationErrorCode implements ErrorCode {
 
     NOT_FOUND_ACCOMMODATION(HttpStatus.NOT_FOUND, "없는 숙박 ID 입니다."),
+    NOT_BOTH_AREA_CATEGORY(HttpStatus.BAD_REQUEST, "동시에는 조회할 수 없습니다."),
+    NOT_FOUND_CATEGORY(HttpStatus.NOT_FOUND, "없는 숙박 종류 입니다."),
+    NOT_FOUND_AREA(HttpStatus.NOT_FOUND, "없는 숙박 위치 입니다."),
+    NOT_FOUND_DATA_PAGE(HttpStatus.NOT_FOUND, "데이터가 없는 페이지입니다."),
     ;
 
     private final HttpStatus code;
