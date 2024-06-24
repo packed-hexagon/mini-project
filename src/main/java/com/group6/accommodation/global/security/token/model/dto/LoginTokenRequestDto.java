efd2f5b683c18bcd8287e48ca48ab824e6d2e458
qudwns017 <qudwns017@naver.com>
2024-06-24 10:17:35 +0900

    Fix: LoginTokenRequestDto 인스턴스 생성 오류 해결

--- src/main/java/com/group6/accommodation/global/security/token/model/dto/LoginTokenRequestDto.java
+++ src/main/java/com/group6/accommodation/global/security/token/model/dto/LoginTokenRequestDto.java
@@ -2,9 +2,11 @@ package com.group6.accommodation.global.security.token.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginTokenRequestDto {
     private String email;
     private String password;
