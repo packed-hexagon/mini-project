7351a1837e4c4152c6eef0eca69d31d284ffe5f3
Hyeonjoon <kingpark02@naver.com>
2024-06-25 11:20:54 +0900

    Refactor: Accommodation 에러 코드 추가

--- src/main/java/com/group6/accommodation/global/exception/error/AccommodationErrorCode.java
+++ src/main/java/com/group6/accommodation/global/exception/error/AccommodationErrorCode.java
@@ -13,6 +13,9 @@ public enum AccommodationErrorCode implements ErrorCode {
     NOT_FOUND_CATEGORY(HttpStatus.NOT_FOUND, "없는 숙박 종류 입니다."),
     NOT_FOUND_AREA(HttpStatus.NOT_FOUND, "없는 숙박 위치 입니다."),
     NOT_FOUND_DATA_PAGE(HttpStatus.NOT_FOUND, "데이터가 없는 페이지입니다."),
+    NOT_FOUND_KEYWORD_ACCOMMODATION(HttpStatus.NOT_FOUND, "해당 키워드가 속한 숙박시설이 없습니다."),
+    ERROR_URI(HttpStatus.BAD_REQUEST, "잘못된 URI입니다."),
+    ERROR_RESTEMPLATE(HttpStatus.BAD_REQUEST, "RestTemplate에 문제가 생겼습니다.")
     ;
 
     private final HttpStatus code;
