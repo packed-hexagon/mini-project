91df2bc1d226520c0f7da52c838bee901bca919f
jisun.park <pjs981114@naver.com>
2024-06-25 16:13:34 +0900

    Feat: userId 시큐리티 적용

--- src/main/java/com/group6/accommodation/domain/likes/controller/UserLikeController.java
+++ src/main/java/com/group6/accommodation/domain/likes/controller/UserLikeController.java
@@ -4,10 +4,12 @@ import com.group6.accommodation.domain.accommodation.model.dto.AccommodationDto;
 import com.group6.accommodation.domain.likes.model.dto.UserLikeResponseDto;
 import com.group6.accommodation.domain.likes.service.UserLikeService;
 import com.group6.accommodation.global.model.dto.PagedDto;
+import com.group6.accommodation.global.security.service.CustomUserDetails;
 import com.group6.accommodation.global.util.ResponseApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,34 +28,34 @@ public class UserLikeController {
     // 숙박시설 찜하기
     @PostMapping("/{accommodationId}")
     public ResponseEntity<ResponseApi<UserLikeResponseDto>> addLike (
-        @PathVariable("accommodationId") Long accommodationID
-//        @AuthenticationPrincipal User user
+        @PathVariable("accommodationId") Long accommodationID,
+        @AuthenticationPrincipal CustomUserDetails user
     ) {
-//        var loginUserId = Long.parseLong(user.getUserId());
-        ResponseApi<UserLikeResponseDto> response = userLikeService.addLike(accommodationID, 1L);
+        var loginUserId = user.getUserId();
+        ResponseApi<UserLikeResponseDto> response = userLikeService.addLike(accommodationID, loginUserId);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     // 숙박시설 찜 삭제하기
     @DeleteMapping("/{accommodationId}")
     public ResponseEntity<ResponseApi<String>> cancelLike(
-        @PathVariable("accommodationId") Long accommodationID
-//        @AuthenticationPrincipal User user
+        @PathVariable("accommodationId") Long accommodationID,
+        @AuthenticationPrincipal CustomUserDetails user
     ) {
-//        var loginUserId = Long.parseLong(user.getUserId());
-        ResponseApi<String> response = userLikeService.cancelLike(accommodationID, 1L);
+        var loginUserId = user.getUserId();
+        ResponseApi<String> response = userLikeService.cancelLike(accommodationID, loginUserId);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     // 찜한 숙박 목록
     @GetMapping
     public ResponseEntity<ResponseApi<PagedDto<AccommodationDto>>> getLikedAccommodation(
-//        @AuthenticationPrincipal User user
+        @AuthenticationPrincipal CustomUserDetails user,
         @RequestParam(name ="page", defaultValue = "0") int page,
         @RequestParam(name = "size", defaultValue = "5") int size
     ) {
-//        var loginUserId = Long.parseLong(user.getUserId());
-        ResponseApi<PagedDto<AccommodationDto>> response = userLikeService.getLikedAccommodation(1L, page, size);
+        var loginUserId = user.getUserId();
+        ResponseApi<PagedDto<AccommodationDto>> response = userLikeService.getLikedAccommodation(loginUserId, page, size);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
