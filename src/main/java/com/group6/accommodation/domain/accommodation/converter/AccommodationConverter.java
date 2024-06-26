6c518791babe81079e311dda281cff7528d879a9
Hyeonjoon <kingpark02@naver.com>
2024-06-26 14:07:14 +0900

    Refactor: 임시 가격 넣는 방법 수정

--- src/main/java/com/group6/accommodation/domain/accommodation/converter/AccommodationConverter.java
+++ src/main/java/com/group6/accommodation/domain/accommodation/converter/AccommodationConverter.java
@@ -1,7 +1,6 @@
 package com.group6.accommodation.domain.accommodation.converter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.group6.accommodation.domain.accommodation.config.OpenapiConfig;
@@ -10,10 +9,13 @@ import com.group6.accommodation.domain.accommodation.model.dto.AccommodationResp
 import com.group6.accommodation.domain.accommodation.model.entity.AccommodationEntity;
 import com.group6.accommodation.domain.accommodation.model.enums.Area;
 import com.group6.accommodation.domain.accommodation.model.enums.Category;
+import com.group6.accommodation.global.exception.error.AccommodationErrorCode;
+import com.group6.accommodation.global.exception.type.AccommodationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
@@ -82,46 +84,42 @@ public class AccommodationConverter {
             ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
 
             if (responseEntity.getStatusCode() == HttpStatus.OK) {
-                // JSON 응답을 로그에 출력
                 String responseBody = responseEntity.getBody();
-                System.out.println("API Response: " + responseBody);
-
                 JsonNode rootNode = objectMapper.readTree(responseBody);
                 JsonNode itemsNode = rootNode.path("response").path("body").path("items").path("item");
 
-                // itemsNode가 배열인지 확인
+                // 객실 정보 자체가 존재하는지
                 if (itemsNode.isArray()) {
                     int minPrice = Integer.MAX_VALUE;
 
                     for (JsonNode itemNode : itemsNode) {
                         int roomPrice = itemNode.path("roomoffseasonminfee1").asInt();
-                        System.out.println("Room Price: " + roomPrice); // 각 객실의 가격을 로그에 출력
+
                         if (roomPrice < minPrice) {
                             minPrice = roomPrice;
                         }
                     }
 
+                    // 가격 정보가 0 으로만 존재한다면
                     if (minPrice == 0) {
                         return tempPrice;
                     } else {
                         return minPrice;
                     }
-                } else if(itemsNode.equals("")) {
+                }
+                // 객실 정보가 존재하지 않으면 임시 가격
+                else {
                     return tempPrice;
-                } else {
-                    System.out.println("Items node is not an array");
                 }
             }
         } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        } catch (JsonMappingException e) {
-            throw new RuntimeException(e);
+            throw new AccommodationException(AccommodationErrorCode.ERROR_URI); // URI가 잘못된 경우
+        } catch (RestClientException e) {
+            throw new AccommodationException(AccommodationErrorCode.ERROR_RESTEMPLATE); // RestTemplate 호출에 문제가 있는 경우
         } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+            throw new AccommodationException(AccommodationErrorCode.ERROR_JSON_PARSING); // Json에 문제가 생겼을 경우
         }
 
         return tempPrice;
     }
-
-
 }
