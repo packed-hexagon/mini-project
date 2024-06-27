2865d0465679bbdb9916f91a2b86abd497f5a5fd
qudwns017 <qudwns017@naver.com>
2024-06-27 10:34:34 +0900

    Feat: 에러 핸들링 컨트롤러 확장

--- src/main/java/com/group6/accommodation/global/security/exception/NotFoundErrorController.java
+++ src/main/java/com/group6/accommodation/global/security/exception/NotFoundErrorController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,17 +19,18 @@ public class NotFoundErrorController implements ErrorController {
     public ResponseEntity<?> handleNotFound(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
-            Map<String, String> message = new HashMap<>();
-            message.put("message", "요청하신 페이지는 찾을 수 없는 페이지입니다.");
+        int resultCode = Integer.parseInt(status.toString());
+        String message = Objects.requireNonNull(HttpStatus.resolve(resultCode)).getReasonPhrase();
 
-            int resultCode = Integer.parseInt(status.toString());
+        Map<String, String> data = new HashMap<>();
+        data.put("message", message);
 
-            SecurityExceptionDto response = SecurityExceptionDto.builder()
-                    .resultCode(resultCode)
-                    .resultMessage(HttpStatus.resolve(resultCode).getReasonPhrase())
-                    .data(message)
-                    .build();
+        SecurityExceptionDto<Object> response = SecurityExceptionDto.builder()
+                .resultCode(resultCode)
+                .resultMessage(message)
+                .data(data)
+                .build();
 
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        return ResponseEntity.status(HttpStatus.valueOf(resultCode)).body(response);
     }
 }
