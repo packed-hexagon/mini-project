7b85f8809717ea0cf625d7713a8f550208f7a0d0
윾승도 <yus174113@gmail.com>
2024-07-01 08:43:20 +0900

    Feat : fetch 로그 추가

--- src/main/java/com/group6/accommodation/domain/room/service/ApiProcessRoomService.java
+++ src/main/java/com/group6/accommodation/domain/room/service/ApiProcessRoomService.java
@@ -6,8 +6,10 @@ import com.group6.accommodation.domain.room.repository.RoomRepository;
 import java.net.URISyntaxException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ApiProcessRoomService {
@@ -18,6 +20,7 @@ public class ApiProcessRoomService {
 
 	public void processRooms() throws URISyntaxException, JsonProcessingException {
 		List<RoomEntity> rooms = roomApiService.fetchAllRooms();
+		log.info("Fetched : {}", rooms.size());
 		roomService.saveRooms(rooms);
 	}
 
