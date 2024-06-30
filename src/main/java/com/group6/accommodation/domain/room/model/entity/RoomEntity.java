aade3c6b254179cbe3d23866051959c2a6b1fd05
윾승도 <yus174113@gmail.com>
2024-06-30 21:32:32 +0900

    Fix : Room 엔티티 수정

--- src/main/java/com/group6/accommodation/domain/room/model/entity/RoomEntity.java
+++ src/main/java/com/group6/accommodation/domain/room/model/entity/RoomEntity.java
@@ -3,23 +3,17 @@ package com.group6.accommodation.domain.room.model.entity;
 import com.group6.accommodation.domain.accommodation.model.entity.AccommodationEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 import java.time.Instant;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 @Getter
 @Setter
@@ -63,7 +57,7 @@ public class RoomEntity {
 	private Integer roomPeakseasonMinfee2;
 
 	@Lob
-	@Column(name = "room_intro")
+	@Column(name = "room_intro", columnDefinition = "TEXT")
 	private String roomIntro;
 
 	@Column(name = "room_bath", length = 32)
@@ -120,10 +114,10 @@ public class RoomEntity {
 	@Column(name = "room_img5", length = 300)
 	private String roomImg5;
 
-	@Column(name = "check_in", nullable = false)
+	@Column(name = "check_in")
 	private Instant checkIn;
 
-	@Column(name = "check_out", nullable = false)
+	@Column(name = "check_out")
 	private Instant checkOut;
 
 }
