cd256fa027038a1ddf4aa894a1f6c8d39786e07e
윾승도 <yus174113@gmail.com>
2024-07-03 13:19:22 +0900

    Feat : max age 추가

--- src/main/java/com/group6/accommodation/global/security/config/CorsConfig.java
+++ src/main/java/com/group6/accommodation/global/security/config/CorsConfig.java
@@ -15,6 +15,7 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of("http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.setMaxAge(1728000L);
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
 
