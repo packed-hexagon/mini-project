ef59978375e4cc31d78a79c4f927dfda9c36671a
TaeMin Kim <gimtaemin36@gmail.com>
2024-07-03 18:53:46 +0900

    Fix: findByStartDateBeforeOrEndDateAfter return타입 List<Long>으로 변경

--- src/main/java/com/group6/accommodation/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/group6/accommodation/domain/reservation/repository/ReservationRepository.java
@@ -4,6 +4,7 @@ import com.group6.accommodation.domain.accommodation.model.entity.AccommodationE
 import com.group6.accommodation.domain.reservation.model.entity.ReservationEntity;
 import com.group6.accommodation.domain.room.model.entity.RoomEntity;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
