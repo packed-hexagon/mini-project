76d487a157b20e3d6d61037ef52ae33e6830dc00
Hyeonjoon <kingpark02@naver.com>
2024-07-03 16:26:03 +0900

    Refactor: 파라미터 -> Dto로 수정

--- src/main/java/com/group6/accommodation/domain/accommodation/controller/AccommodationController.java
+++ src/main/java/com/group6/accommodation/domain/accommodation/controller/AccommodationController.java
@@ -1,26 +1,22 @@
 package com.group6.accommodation.domain.accommodation.controller;
 
+import com.group6.accommodation.domain.accommodation.model.dto.AccommodationConditionRequest;
 import com.group6.accommodation.domain.accommodation.model.dto.AccommodationDetailResponseDto;
 import com.group6.accommodation.domain.accommodation.model.dto.AccommodationResponseDto;
 import com.group6.accommodation.domain.accommodation.annotation.ValidArea;
 import com.group6.accommodation.domain.accommodation.annotation.ValidCategory;
-import com.group6.accommodation.domain.accommodation.model.entity.AccommodationEntity;
 import com.group6.accommodation.global.model.dto.PagedDto;
 import com.group6.accommodation.domain.accommodation.service.AccommodationService;
-import com.group6.accommodation.global.util.Response;
 import com.group6.accommodation.global.util.ResponseApi;
-import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.Min;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.util.List;
-
 @RestController
+@Validated
 @RequiredArgsConstructor
 @RequestMapping("/open-api")
 public class AccommodationController {
@@ -62,18 +58,10 @@ public class AccommodationController {
     // 위치, 날짜 범위, 인원 수 조건을 유동적으로 받아와 숙소 조회
     @GetMapping("/accommodation/condition")
     public ResponseEntity<ResponseApi<PagedDto<AccommodationResponseDto>>> searchAccommodations(
-            @RequestParam(required = false)
-            String area,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @FutureOrPresent(message = "시작날짜가 과거일 수 없다.")
-            LocalDate startDate,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @FutureOrPresent(message = "종료날짜가 과거일 수 없다.")
-            LocalDate endDate,
-            @RequestParam(required = false) @Min(value = 1, message = "인원 수는 1명 이상이어야 합니다.")
-            Integer headcount,
-            @RequestParam(defaultValue = "0")
-            int page) {
+            @Valid AccommodationConditionRequest request) {
 
-        PagedDto<AccommodationResponseDto> accommodationPage = accommodationService.findAvaliableAccommodation(area, startDate, endDate, headcount, page);
+        PagedDto<AccommodationResponseDto> accommodationPage = accommodationService.findAvaliableAccommodation(
+                request.getArea(), request.getStartDate(), request.getEndDate(), request.getHeadcount(), request.getPage());
 
         return ResponseEntity.ok(ResponseApi.success(HttpStatus.OK, accommodationPage));
     }
---
+++ src/main/java/com/group6/accommodation/domain/accommodation/model/dto/AccommodationConditionRequest.java
@@ -0,0 +1,32 @@
+package com.group6.accommodation.domain.accommodation.model.dto;
+
+import com.group6.accommodation.domain.accommodation.annotation.ValidArea;
+import com.group6.accommodation.domain.reservation.annotation.StartBeforeEnd;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Min;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@StartBeforeEnd(startTime = "startDate", endTime = "endDate")
+public class AccommodationConditionRequest {
+    @ValidArea
+    String area;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @FutureOrPresent(message = "시작날짜가 과거일 수 없다.")
+    LocalDate startDate;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @FutureOrPresent(message = "종료날짜가 과거일 수 없다.")
+    LocalDate endDate;
+
+    @Min(value = 1, message = "인원 수는 1명 이상이어야 합니다.")
+    Integer headcount;
+
+    int page = 0;
+}
