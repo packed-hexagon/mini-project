95acd1ca9277405de715032c6f4bac45567388cf
윾승도 <yus174113@gmail.com>
2024-07-05 22:47:36 +0900

    Fix : 누락된 메서드 추가

--- src/main/java/com/group6/accommodation/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/group6/accommodation/domain/reservation/repository/ReservationRepository.java
@@ -3,13 +3,11 @@ package com.group6.accommodation.domain.reservation.repository;
 import com.group6.accommodation.domain.accommodation.model.entity.AccommodationEntity;
 import com.group6.accommodation.domain.reservation.model.entity.ReservationEntity;
 import com.group6.accommodation.domain.room.model.entity.RoomEntity;
-import jakarta.persistence.LockModeType;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -36,5 +34,9 @@ public interface ReservationRepository extends JpaRepository<ReservationEntity, 
     int countOverlappingReservations(@Param("roomId") Long roomId,
         @Param("checkIn") LocalDate checkIn,
         @Param("checkOut") LocalDate checkOut);
+
+
+    @Query("SELECT r.room.roomId FROM ReservationEntity r WHERE r.startDate <= :endDate AND r.endDate >= :startDate AND r.deletedAt IS NULL")
+    List<Long> findByStartDateBeforeOrEndDateAfter(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
 }
