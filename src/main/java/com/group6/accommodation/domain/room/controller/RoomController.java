b7bba99475a9e7ec1f0efbc1a48be2094ca6dfce
윾승도 <yus174113@gmail.com>
2024-07-07 02:28:06 +0900

    HOTFIX : /accommodation/{id}/room/{roomId}/is-reservable -> post 요청으로 수정

--- src/main/java/com/group6/accommodation/domain/room/controller/RoomController.java
+++ src/main/java/com/group6/accommodation/domain/room/controller/RoomController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,7 +42,7 @@ public class RoomController {
 		return ResponseEntity.ok(ResponseApi.success(HttpStatus.OK, roomDto));
 	}
 
-	@GetMapping("/accommodation/{id}/room/{roomId}/is-reservable")
+	@PostMapping("/accommodation/{id}/room/{roomId}/is-reservable")
 	public ResponseEntity<ResponseApi<AvailableRoomsRes>> availableRooms(
 		@PathVariable Long id,
 		@PathVariable Long roomId,
