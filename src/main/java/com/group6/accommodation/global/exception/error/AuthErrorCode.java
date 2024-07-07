dceb59809f44825bbd5ae7013cf6fb5c8db4094d
qudwns017 <qudwns017@naver.com>
2024-07-07 16:31:46 +0900

    Feat: Token Error Status Code 변경

--- src/main/java/com/group6/accommodation/global/exception/error/AuthErrorCode.java
+++ src/main/java/com/group6/accommodation/global/exception/error/AuthErrorCode.java
@@ -13,11 +13,11 @@ public enum AuthErrorCode implements ErrorCode {
     NOT_FOUND_USER_BY_EMAIL(HttpStatus.NOT_FOUND, "해당 이메일을 가진 유저가 없습니다."),
     NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 틀렸습니다."),
     NOT_FOUNT_USER_BY_USER_ID(HttpStatus.NOT_FOUND, "해당 ID를 가진 유저가 없습니다."),
-    INVALID_TOKEN(HttpStatus.NOT_FOUND, "유효하지 않은 토큰입니다."),
-    EMPTY_TOKEN(HttpStatus.NOT_FOUND, "토큰이 비어있습니다."),
-    EXPIRED_TOKEN(HttpStatus.NOT_FOUND, "토큰이 만료되었습니다."),
-    UNSUPPORTED_TOKEN_TYPE(HttpStatus.NOT_FOUND, "올바르지 않은 토큰 인증 방식입니다."),
-    UNKNOWN_AUTH_ERROR(HttpStatus.BAD_REQUEST, "알 수 없는 토큰 오류입니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+    EMPTY_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 비어있습니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
+    UNSUPPORTED_TOKEN_TYPE(HttpStatus.UNAUTHORIZED, "올바르지 않은 토큰 인증 방식입니다."),
+    UNKNOWN_AUTH_ERROR(HttpStatus.UNAUTHORIZED, "알 수 없는 토큰 오류입니다."),
     ALREADY_LOGOUT(HttpStatus.BAD_REQUEST, "이미 로그아웃 된 유저입니다."),
     ;
 
