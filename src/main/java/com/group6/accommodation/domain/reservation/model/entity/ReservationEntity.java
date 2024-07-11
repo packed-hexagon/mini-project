fb3a2eea16ac397bbc66546a2161ab7a4df57366
윾승도 <yus174113@gmail.com>
2024-07-11 18:08:02 +0900

    Fix : ReservationEntity 수정

--- src/main/java/com/group6/accommodation/domain/reservation/model/entity/ReservationEntity.java
+++ src/main/java/com/group6/accommodation/domain/reservation/model/entity/ReservationEntity.java
@@ -1,9 +1,7 @@
 package com.group6.accommodation.domain.reservation.model.entity;
 
-import com.group6.accommodation.domain.accommodation.model.entity.AccommodationEntity;
 import com.group6.accommodation.domain.auth.model.entity.UserEntity;
 import com.group6.accommodation.domain.room.model.entity.RoomEntity;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -16,7 +14,6 @@ import jakarta.persistence.Table;
 import java.time.Instant;
 import java.time.LocalDate;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -32,29 +29,25 @@ public class ReservationEntity {
 	@Column(name = "reservation_id", nullable = false)
 	private Long reservationId;
 
-	@ManyToOne(cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="user_id", referencedColumnName = "user_id")
 	private UserEntity user;
 
-	@ManyToOne(cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
-	@JoinColumn(name="accommodation_id", referencedColumnName = "accommodation_id")
-	private AccommodationEntity accommodation;
-
-	@ManyToOne(cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="room_id", referencedColumnName = "room_id")
 	private RoomEntity room;
 
 	@Column(name = "headcount", nullable = false)
 	private Integer headcount;
 
+	@Column(name = "price", nullable = false)
+	private Integer price;
+
 	@Column(name = "start_date", nullable = false)
 	private LocalDate startDate;
 
 	@Column(name = "end_date", nullable = false)
 	private LocalDate endDate;
-
-	@Column(name = "price", nullable = false)
-	private Integer price;
 
 	@CreationTimestamp
 	@Column(name = "created_at", nullable = false)
@@ -63,16 +56,4 @@ public class ReservationEntity {
 	@Setter
 	@Column(name = "deleted_at")
 	private Instant deletedAt;
-
-	@Builder
-	public ReservationEntity(UserEntity user, AccommodationEntity accommodation, RoomEntity room, int headcount, LocalDate startDate, LocalDate endDate, Integer price) {
-		this.user = user;
-		this.accommodation = accommodation;
-		this.room = room;
-		this.headcount = headcount;
-		this.startDate = startDate;
-		this.endDate = endDate;
-		this.price = price;
-	}
-
 }
