253db6e2b3c5cec10775a2932796260dd4287487
Hyeonjoon <kingpark02@naver.com>
2024-07-19 16:48:18 +0900

    Refactor: Accommodation Dto로 변환해주는 정적 팩토리 메서드 생성(#25)

--- src/main/java/com/group6/accommodation/domain/accommodation/model/dto/AccommodationDetailResponseDto.java
+++ src/main/java/com/group6/accommodation/domain/accommodation/model/dto/AccommodationDetailResponseDto.java
@@ -1,5 +1,9 @@
 package com.group6.accommodation.domain.accommodation.model.dto;
 
+import com.group6.accommodation.domain.accommodation.model.entity.AccommodationEntity;
+import com.group6.accommodation.domain.accommodation.model.enums.Area;
+import com.group6.accommodation.domain.accommodation.model.enums.Category;
+import com.group6.accommodation.domain.accommodation.service.AccommodationService;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,11 +22,30 @@ public class AccommodationDetailResponseDto {
     private String category;
     private String image;
     private String thumbnail;
-    private int price;
+    private Integer price;
     private double latitude;
     private double longitude;
     private String tel;
     private int likeCount;
     private int reviewCount;
     private double totalRating;
+
+    public static AccommodationDetailResponseDto fromEntity(AccommodationEntity entity, AccommodationService accommodationService) {
+        return new AccommodationDetailResponseDto(
+                entity.getId(),
+                entity.getTitle(),
+                entity.getAddress(),
+                Area.getNameByCode(entity.getAreacode()),
+                Category.getNameByCode(entity.getCategory()),
+                entity.getImage(),
+                entity.getThumbnail(),
+                accommodationService.getMinRoomPrice(entity.getId()),
+                entity.getLatitude(),
+                entity.getLongitude(),
+                entity.getTel(),
+                entity.getLikeCount(),
+                entity.getReviewCount(),
+                entity.getTotalRating()
+        );
+    }
 }
